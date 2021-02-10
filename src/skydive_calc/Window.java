bacbc4657a36b38f6abd6c4692c1da4f2e7174be
Neo Argatides <neo.argatides@gmail.com>
2021-02-09 19:31:22 -0800

    map works

--- src/skydive_calc/Window.java
+++ src/skydive_calc/Window.java
@@ -1,12 +1,20 @@
 package skydive_calc;
 
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Window extends JFrame
 {
 	private static final long serialVersionUID = 1L;
-	String map = "map.png";
+	String map = "src/skydive_calc/map.png";
 	
 	public Window() 
 	{
@@ -19,14 +27,9 @@ public class Window extends JFrame
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	    setSize(screenSize.width, screenSize.height);
 	    
-	    add(imagePanel);
+	    int width = (int)Math.round(screenSize.height * (203/227));
+	    JLabel imageLabel = new JLabel();
+	    imageLabel.setIcon(new ImageIcon(new ImageIcon(map).getImage().getScaledInstance(939, screenSize.height, Image.SCALE_DEFAULT)));
+	    add(imageLabel);
 	}
-	
-	JPanel imagePanel = new JPanel()
-	{
-	    public void paintComponent(Graphics g)
-	    {
-	    	
-	    }
-	};
 }
