006e0e72829fe170b290249b66257858035be9a6
Neo Argatides <neo.argatides@gmail.com>
2021-02-13 17:57:18 -0800

    map buttons

--- src/skydive_calc/Window.java
+++ src/skydive_calc/Window.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
@@ -37,11 +39,56 @@ public class Window extends JFrame
 	    
 	    JLabel imageLabel = new JLabel();
 	    imageLabel.setVerticalAlignment(SwingConstants.TOP);
-	    imageLabel.setIcon(new ImageIcon(new ImageIcon(map).getImage().getScaledInstance(939, mainPanel.getHeight(), Image.SCALE_SMOOTH)));
 	    mainPanel.add(imageLabel, BorderLayout.LINE_START);
+	    imageLabel.setIcon(new ImageIcon(new ImageIcon(map).getImage().getScaledInstance(812, 908, Image.SCALE_SMOOTH)));
 	    
 	    JPanel optionPanel = new JPanel();
 	    optionPanel.setLayout(new BoxLayout(optionPanel, BoxLayout.PAGE_AXIS));
+	    mainPanel.add(optionPanel, BorderLayout.LINE_END);
+	    
+	    JButton mapBtn = new JButton("Clear Map");
+	    optionPanel.add(mapBtn);
+	    mapBtn.addActionListener(new ActionListener()
+        {
+        	public void actionPerformed(ActionEvent e)
+        	{
+        		map = "src/skydive_calc/map.png";
+        		imageLabel.setIcon(new ImageIcon(new ImageIcon(map).getImage().getScaledInstance(812, 908, Image.SCALE_SMOOTH)));
+        	}
+        });
+	    
+	    JButton anchorBtn = new JButton("Anchor Map");
+	    optionPanel.add(anchorBtn);
+	    anchorBtn.addActionListener(new ActionListener()
+        {
+        	public void actionPerformed(ActionEvent e)
+        	{
+        		map = "src/skydive_calc/anchor_map.png";
+        		imageLabel.setIcon(new ImageIcon(new ImageIcon(map).getImage().getScaledInstance(812, 908, Image.SCALE_SMOOTH)));
+        	}
+        });
+	    
+	    JButton solidBtn = new JButton("Solids Map");
+	    optionPanel.add(solidBtn);
+	    solidBtn.addActionListener(new ActionListener()
+        {
+        	public void actionPerformed(ActionEvent e)
+        	{
+        		map = "src/skydive_calc/solid_map.png";
+        		imageLabel.setIcon(new ImageIcon(new ImageIcon(map).getImage().getScaledInstance(812, 908, Image.SCALE_SMOOTH)));
+        	}
+        });
+	    
+	    JButton shardBtn = new JButton("Shard Map");
+	    optionPanel.add(shardBtn);
+	    shardBtn.addActionListener(new ActionListener()
+        {
+        	public void actionPerformed(ActionEvent e)
+        	{
+        		map = "src/skydive_calc/shard_map.png";
+        		imageLabel.setIcon(new ImageIcon(new ImageIcon(map).getImage().getScaledInstance(812, 908, Image.SCALE_SMOOTH)));
+        	}
+        });
 	    
 	}
 	
